138fb0822dca2671fec565bde53c645b9d521bf6
crlotwhite <crlotwhite@gmail.com>
2021-10-19 02:32:04 +0800

    Fix npm start

--- app.js
+++ app.js
@@ -20,4 +20,4 @@ app.use('/', indexRouter);
 app.use('/', uploadRouter);
 app.use('/users', usersRouter);
 
-module.exports = app;
+app.listen(3000, () => console.log('Example app listening on port 3000!'));
